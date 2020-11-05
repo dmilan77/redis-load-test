e987bea69bbba67f5caed6f01ed74bf552deae17
Milan Das <milan@ulap.co>
2020-11-05 17:02:52 -0500

    added redis-cli

--- test_sentinel.py
+++ test_sentinel.py
@@ -13,10 +13,13 @@ def redis_populate():
   #     ("redis-cloudflare-node-2.redis-cloudflare-headless.dns-proxy.svc.cluster.local", 26379)
   #   ],     
   # socket_timeout=0.1)
-  sentinel = Sentinel(["redis-cloudflare:26379"], socket_timeout = None, sentinel_kwargs={'password': 'NotImPortAntPassWorD'})
+  # sentinel = Sentinel(["redis-cloudflare:26379"], socket_timeout = None, sentinel_kwargs={'password': 'NotImPortAntPassWorD'})
   # password="NotImPortAntPassWorD", sentinel_kwargs={"password": "NotImPortAntPassWorD"})
-
+  sentinel = Sentinel([
+    ('redis-cloudflare:26379')
+    ],sentinel_kwargs={'password': 'NotImPortAntPassWorD'})   
   sentinel.discover_master('mymaster')
+
   client = sentinel.master_for('mymaster', socket_timeout=0.5)
   for i in range(100000):
       key='key'+str(i)
